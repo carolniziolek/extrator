e6d8c01252f086cce808c35e81968b2c08bbfa7d
carolniziolek <caroline.niziolek.oliveira@escola.pr.gov.br>
2025-08-07 11:43:59 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const botãoMostraPalavras= document.querySelector('#botao-palavrachave');
+botãoMostraPalavras,addEventListener('click',mostraPalavrasChaves);
+function mostraPalavrasChaves(){
+    alert('fui clicado')
+}
